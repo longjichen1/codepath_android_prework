0b17eec9de8fbe24465cf6685b8bd5e85f68ef0c
dobirf <47933146+dobirf@users.noreply.github.com>
2022-01-30 15:59:37 -0600

    fully funcitoning basic app

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -2,28 +2,37 @@ package com.example.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Button
 import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
 
-    val listOfTasks = mutableListOf<String>()
+    var listOfTasks = mutableListOf<String>()
     lateinit var adapter: TaskItemAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
+        loadItems()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val onLongClickListener = object : TaskItemAdapter.OnLongClickListener {
             override fun onItemLongClicked(position: Int) {
+                // 1. Remove item from list
                 listOfTasks.removeAt(position)
 
+                // 2. Notify adapter
                 adapter.notifyDataSetChanged()
+
+                // 3. Save File
+                saveItems()
             }
         }
-
-        listOfTasks.add("do")
 
         // Look up recyclerView in layout
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
@@ -48,6 +57,36 @@ class MainActivity : AppCompatActivity() {
 
             // 3. Reset text field
             inputTextField.setText("")
+
+            // 4. Save File
+            saveItems()
+        }
+    }
+    // Save data that user inputted
+    // Save data by writing and reading from a file
+
+    // Create a method to get the file we need
+    fun getDataFile() : File {
+
+        // Every lien is going to represent a specific task in our list of tasks
+        return File(filesDir, "data.txt")
+    }
+
+    // Load the items by reading every line in the data file
+    fun loadItems() {
+        try{
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        } catch (ioException: IOException){
+            ioException.printStackTrace()
+        }
+    }
+
+    // Save items by writing them into our data file
+    fun saveItems() {
+        try{
+            FileUtils.writeLines(getDataFile(), listOfTasks)
+        } catch (ioException: IOException){
+            ioException.printStackTrace()
         }
     }
 }
